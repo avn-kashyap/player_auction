ead6d3f3d3a5fff195d8d943f297af2367310fa7
Avinash kumar <avnkashyap@gmail.com>
2020-04-23 05:13:31 +0530

    modified utility.py and dao.py

--- Utility.py
+++ Utility.py
@@ -1,10 +1,15 @@
 import Exceptions
 import mysql.connector
+import configparser
 
+config = configparser.ConfigParser()
+config.read('./config.ini')
 def getconnection():
     try:
-        connection = mysql.connector.connect(user='*****',password='*****'
-                                      ,host='******',database='*****')
+        connection = mysql.connector.connect(user=config.get('database_server','user'),
+                     password=config.get('database_server','password')
+                    ,host=config.get('database_server','host'),
+                    database=config.get('database_server','database'))
     except Exception:
         raise Exceptions.DaoExceptions("Connection to the database failed!!!!!!")
     else:
